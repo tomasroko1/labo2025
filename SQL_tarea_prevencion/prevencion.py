f3698e437f2ef24ae1f4b5e58f50ff77d03d4816
tomas <tomas.e.roko@gmail.com>
2025-02-10 11:02:05 -0300

    'push'

--- SQL_tarea_prevencion/prevencion.py
+++ SQL_tarea_prevencion/prevencion.py
@@ -8,4 +8,33 @@ Created on Wed Feb  5 23:07:30 2025
 import pandas as pd
 import duckdb as dd
 
-#%%
+#%% PERDÍ TODO HASTA EL D j  :((((((((((((( 
+
+casos = pd.read_csv('casos.csv')
+
+departamento = pd.read_csv('departamento.csv')
+
+grupoetario = pd.read_csv('grupoetario.csv')
+
+provincia = pd.read_csv('provincia.csv')
+
+tipoevento = pd.read_csv('tipoevento.csv')
+
+# ARRANCO DESDE EL E
+
+#%% # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+ # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+ # #                                                                     # #
+# #    INICIO -->           EJERCICIO Nro. E                              # #
+ # #                                                                     # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+ # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+
+
+
+
